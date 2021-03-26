6dc5412175b0d1da63d1bdc22ab840360c57e00c
zhuzonbir <80466692+zhuzonbir@users.noreply.github.com>
2021-03-26 23:20:31 +0600

    Add files via upload

---
+++ 1.dart
@@ -0,0 +1,47 @@
+void main() {
+//   int a = 35;
+//   int b = 1398;
+
+  
+
+//   print(b/a); // obychnoe delenie
+
+//   int c = 44;
+//   int d = 45;
+//   int e = 456;
+//   print ((d*c)-e);//chutok slojnee uravnenie opyt exel pomog =))
+
+//   int f = 4564;
+//   int g = 45666;
+
+// print("esli vot eto:$f umnojit na vot eto:$g");
+// print("budet");
+// print(f * g); //navernoe v proggramirovanii tak ne delaetsa, no u menya poluchilos
+
+
+
+// String name = "Nik";
+// String probel = " ";
+// String opisanie = "krasavchik";
+// print(name + probel + opisanie); // zasunul probel dv peremennuyu srazy
+// print(name + " " + opisanie); // ne zasovyval probel
+
+// List array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "poltora", "nik", "tri dnya v nedelyu", "zachem", "chasa", "hochet", "uchit flutter", "potomu chto", "byt", "krasavchikom"];
+// int i = 456;
+// int h = 789;
+// int j = 100;
+// print(array[4]*h); // index pod nomerom 5 umnojayu na h 
+// print((array[0]) + (array[9])); // nol` plus odin` = odin`
+// print(f * (array[1])* (array[5]));// vstavil f  umnojayu na summu pervogo indexa i pyatogo
+// print((array[13])+(probel)+(array[11])+(probel)+(array[12])+(probel)+(array[10])+(probel)+(array[14])+(probel)+(array[16])+(probel)+(array[17])+(probel)+(array[15])+(probel)+(array[18])+(probel)+(array[19]));
+
+bool das = true;
+bool niik = false;
+print(das);
+if(das) 
+  print("name is Dastna");
+if (das)
+niik = true;
+if (niik)
+print("name is nik");
+}
